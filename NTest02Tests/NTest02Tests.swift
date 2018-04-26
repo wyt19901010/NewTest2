95aae866463415e9623d8042f2302c5c3b92412f
WU YUTONG <wyt363466854@gmail.com>
2018-04-26 12:13:07 +0900

    Initial Commit

---
+++ NTest02Tests/NTest02Tests.swift
@@ -0,0 +1,36 @@
+//
+//  NTest02Tests.swift
+//  NTest02Tests
+//
+//  Created by YUTONG WU on 2018/04/26.
+//  Copyright © 2018 Big. All rights reserved.
+//
+
+import XCTest
+@testable import NTest02
+
+class NTest02Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
